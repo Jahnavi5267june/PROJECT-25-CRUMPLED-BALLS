88aa05bc880d7c1a12507e1a96af510c025ebb33
Jahnavi5267june <84388382+Jahnavi5267june@users.noreply.github.com>
2021-06-22 16:27:10 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,23 @@
+class Paper {
+    constructor(x, y,radius) {
+      var options = {
+          isStatic:false,
+          'restitution':0.3,
+          'friction':0,
+          'density':1.2
+      }
+      
+      this.body = Bodies.circle(x,y,radius,options);
+      this.image = loadImage("paper.png")
+      World.add(world, this.body);
+      
+      }
+      display(){
+        
+        var pos =this.body.position;   
+        ellipseMode(RADIUS);
+        circle( this.body.position.x, this.body.position.y,this.radius);
+       
+      }
+};
+
