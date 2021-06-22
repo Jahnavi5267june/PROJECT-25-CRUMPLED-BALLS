fcd7a9e52944c2c78c15cc702ac69c3dc481d58c
Jahnavi5267june <84388382+Jahnavi5267june@users.noreply.github.com>
2021-06-22 17:52:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,9 +19,13 @@ function setup() {
 	world = engine.world;
 	
 	
+	
 	paperObject=new Paper(200,450,60);
 	groundObject=new ground(width/2,670,width,20);
 	dustbinObj=new dustbin(1200,650);
+	
+	
+	var render = Render.create({ element: document.body, engine: engine, options: { width: 1600, height: 700, wireframes: false } });
 	
 	Engine.run(engine);
 	Render.run(render);
